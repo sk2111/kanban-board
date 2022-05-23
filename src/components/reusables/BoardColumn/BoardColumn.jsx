6ee3c345520d3be7361148b8c15b652ec0f6371a
Sathishkumar Thangavel <sathishkumar.thangavel@chargebee.com>
2022-05-23 18:04:11 +0530

    refactor(components): :recycle: board column implementation clean up

--- src/components/reusables/BoardColumn/BoardColumn.jsx
+++ src/components/reusables/BoardColumn/BoardColumn.jsx
@@ -4,37 +4,45 @@ import PropTypes from "prop-types";
 //css
 import styles from "./BoardColumn.module.css";
 //components
-import { Droppable } from "react-beautiful-dnd";
+import { Droppable, Draggable } from "react-beautiful-dnd";
 import BoardTask from "components/reusables/BoardTask/BoardTask";
 
-const BoardColumn = ({ title, columnId, tasks }) => {
+const BoardColumn = ({ column, tasks, index }) => {
   return (
-    <div className={styles.container}>
-      <h3 className={styles.title}>
-        {title} - {tasks.length}
-      </h3>
-      <Droppable droppableId={columnId}>
-        {(provided) => (
-          <div
-            className={styles.taskList}
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            {tasks.map((task, index) => (
-              <BoardTask key={task.id} task={task} index={index} />
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </div>
+    <Draggable draggableId={column.id} index={index}>
+      {(provided) => (
+        <div
+          className={styles.container}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+        >
+          <h2 className={styles.title} {...provided.dragHandleProps}>
+            {column.title}
+          </h2>
+          <Droppable droppableId={column.id} type="task">
+            {(provided) => (
+              <div
+                className={styles.taskList}
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                {tasks.map((task, index) => (
+                  <BoardTask key={task.id} task={task} index={index} />
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </div>
+      )}
+    </Draggable>
   );
 };
 
 BoardColumn.propTypes = {
-  title: PropTypes.string.isRequired,
-  columnId: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  column: PropTypes.object.isRequired,
   tasks: PropTypes.array.isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default BoardColumn;
